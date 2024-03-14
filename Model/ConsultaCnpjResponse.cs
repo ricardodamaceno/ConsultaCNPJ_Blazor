a88a2319d75a753f5c81f4955c27da36160de74c
ricardo <damaceno.ric@gmail.com>
2024-03-13 22:05:27 -0300

    Adição de Classe e campos no Index

---
+++ Model/ConsultaCnpjResponse.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+
+namespace ConsultaCNPJ.Model
+{
+    public class ConsultaCnpjResponse
+    {
+        [JsonProperty("abertura")]
+        public string Abertura { get; set; }
+
+        [JsonProperty("situacao")]
+        public string Situacao { get; set; }
+
+        [JsonProperty("tipo")]
+        public string Tipo { get; set; }
+
+        [JsonProperty("nome")]
+        public string Nome { get; set; }
+
+        [JsonProperty("fantasia")]
+        public string Fantasia { get; set; }
+
+        [JsonProperty("logradouro")]
+        public string Logradouro { get; set; }
+
+        [JsonProperty("numero")]
+        public string Numero { get; set; }
+
+        [JsonProperty("municipio")]
+        public string Municipio { get; set; }
+
+        [JsonProperty("bairro")]
+        public string Bairro { get; set; }
+
+        [JsonProperty("uf")]
+        public string UF { get; set; }
+
+        [JsonProperty("cep")]
+        public string CEP { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("telefone")]
+        public string Telefone { get; set; }
+        
+        [JsonProperty("cnpj")]
+        public string CNPJ { get; set; }
+    }
+}
